569a6e399ff958fa9b82c5cdc8d2f8b1ae31bee8
peatroot <garethpeat@gmail.com>
2018-11-14 09:45:10 +0000

    Move schema to gql file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
 
@@ -5,74 +7,11 @@ import targetSummary from "./resolvers/targetSummary";
 import diseaseSummary from "./resolvers/diseaseSummary";
 import diseaseDAG from "./resolvers/diseaseDAG";
 
-const typeDefs = gql`
-  type Query {
-    targetSummary(ensgId: String!): TargetSummary!
-    diseaseSummary(efoId: String!): DiseaseSummary!
-    diseaseDAG(efoId: String!): DiseaseDAG!
-  }
-  type TargetSummary {
-    id: String!
-    name: String!
-    symbol: String!
-    synonyms: [String!]!
-    drugs: TargetSummaryDrugs!
-    pathways: TargetSummaryPathways!
-    cancerBiomarkers: TargetSummaryCancerBiomarkers!
-    chemicalProbes: TargetSummaryChemicalProbes!
-    protein: TargetSummaryProtein!
-    similarTargets: TargetSummarySimilarTargets!
-  }
-  type DiseaseSummary {
-    id: String!
-    name: String!
-    synonyms: [String!]!
-  }
-  type DiseaseDAG {
-    nodes: [DiseaseDAGNode!]!
-  }
-  type DiseaseDAGNode {
-    id: String!
-    name: String!
-    parentIds: [String!]
-    nodeType: String!
-  }
-  type TargetSummaryDrugs {
-    count: Int!
-    modalities: TargetDrugModalitiesAgg!
-    trialsByPhase: [TargetDrugTrialsByPhaseAgg!]!
-  }
-  type TargetDrugModalitiesAgg {
-    antibody: Boolean!
-    peptide: Boolean!
-    protein: Boolean!
-    smallMolecule: Boolean!
-  }
-  type TargetDrugTrialsByPhaseAgg {
-    phase: Int!
-    trialCount: Int!
-  }
-  type TargetSummaryPathways {
-    count: Int!
-  }
-  type TargetSummaryCancerBiomarkers {
-    count: Int!
-    diseaseCount: Int!
-  }
-  type TargetSummaryChemicalProbes {
-    portalProbeCount: Int!
-    hasProbeMinerLink: Boolean!
-  }
-  type TargetSummaryProtein {
-    hasProtVista: Boolean!
-    subcellularLocation: [String!]
-  }
-  type TargetSummarySimilarTargets {
-    count: Int!
-    averageCommonDiseases: Int!
-  }
-`;
+// load the schema
+const schemaFile = path.join(__dirname, "schema.gql");
+const typeDefs = fs.readFileSync(schemaFile, "utf8");
 
+// create resolver object (mirrors typeDefs)
 const resolvers = {
   Query: {
     targetSummary,
