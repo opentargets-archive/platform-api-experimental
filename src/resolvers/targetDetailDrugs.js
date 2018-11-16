dff90d85ea41d0845c0708d49c636a49b28834f6
peatroot <garethpeat@gmail.com>
2018-11-16 17:02:50 +0000

    Use enums for several drug fields

--- src/resolvers/targetDetailDrugs.js
+++ src/resolvers/targetDetailDrugs.js
@@ -4,6 +4,7 @@ import { targetDrugsIterated } from "../apis/openTargets";
 const MAP_ACTIVITY = {
   drug_positive_modulator: "agonist",
   drug_negative_modulator: "antagonist",
+  up_or_down: "up_or_down",
 };
 
 const targetDetailDrugs = async (obj, { ensgId }) => {
@@ -16,7 +17,7 @@ const targetDetailDrugs = async (obj, { ensgId }) => {
     const efoLabel = d.disease.efo_info.label;
     const drugId = d.drug.id.split("/").pop();
     const drugName = d.drug.molecule_name;
-    const drugType = d.drug.molecule_type;
+    const drugType = d.drug.molecule_type.replace(" ", "_").toUpperCase();
     const mechanismOfAction = d.evidence.target2drug.mechanism_of_action;
     const mechanismOfActionUrl =
       d.evidence.target2drug.urls.length === 3
@@ -27,8 +28,13 @@ const targetDetailDrugs = async (obj, { ensgId }) => {
         ? d.evidence.target2drug.urls[2].nice_name
         : null;
     const phase = d.evidence.drug2clinic.max_phase_for_disease.numeric_index;
-    const status = d.evidence.drug2clinic.status;
-    const activity = MAP_ACTIVITY[d.target.activity];
+    const status = d.evidence.drug2clinic.status
+      ? d.evidence.drug2clinic.status
+          .replace(" ", "_")
+          .replace(",", "")
+          .toUpperCase()
+      : null;
+    const activity = MAP_ACTIVITY[d.target.activity].toUpperCase();
     const evidenceUrl = d.evidence.drug2clinic.urls[0].url;
     const evidenceSource = d.evidence.drug2clinic.urls[0].nice_name.replace(
       " Information",
