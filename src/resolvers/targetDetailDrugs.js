14e73e709c6488e18f0edbb36c31afe6b7a2dd7e
peatroot <garethpeat@gmail.com>
2018-11-16 13:34:50 +0000

    Add evidence and mechanismOfAction

--- src/resolvers/targetDetailDrugs.js
+++ src/resolvers/targetDetailDrugs.js
@@ -8,34 +8,53 @@ const MAP_ACTIVITY = {
 
 const targetDetailDrugs = async (obj, { ensgId }) => {
   const rows = await targetDrugsIterated(ensgId);
+  const rowsFormatted = rows.map(d => {
+    const targetId = d.target.id;
+    const targetSymbol = d.target.gene_info.symbol;
+    const targetClass = d.target.target_class[0];
+    const efoId = d.disease.efo_info.efo_id.split("/").pop();
+    const efoLabel = d.disease.efo_info.label;
+    const drugId = d.drug.id.split("/").pop();
+    const drugName = d.drug.molecule_name;
+    const drugType = d.drug.molecule_type;
+    const mechanismOfAction = d.evidence.target2drug.mechanism_of_action;
+    const mechanismOfActionUrl =
+      d.evidence.target2drug.urls.length === 3
+        ? d.evidence.target2drug.urls[2].url
+        : null;
+    const mechanismOfActionSource =
+      d.evidence.target2drug.urls.length === 3
+        ? d.evidence.target2drug.urls[2].nice_name
+        : null;
+    const phase = d.evidence.drug2clinic.max_phase_for_disease.numeric_index;
+    const status = d.evidence.drug2clinic.status;
+    const activity = MAP_ACTIVITY[d.target.activity];
+    const evidenceUrl = d.evidence.drug2clinic.urls[0].url;
+    const evidenceSource = d.evidence.drug2clinic.urls[0].nice_name.replace(
+      " Information",
+      ""
+    );
+    return {
+      targetId,
+      targetSymbol,
+      targetClass,
+      efoId,
+      efoLabel,
+      drugId,
+      drugName,
+      drugType,
+      phase,
+      status,
+      activity,
+      evidenceUrl,
+      evidenceSource,
+      mechanismOfAction,
+      mechanismOfActionUrl,
+      mechanismOfActionSource,
+    };
+  });
   return {
-    rows: rows.map(d => {
-      const targetId = d.target.id;
-      const targetSymbol = d.target.gene_info.symbol;
-      const targetClass = d.target.target_class[0];
-      const efoId = d.disease.efo_info.efo_id.split("/").pop();
-      const efoLabel = d.disease.efo_info.label;
-      const drugId = d.drug.id.split("/").pop();
-      const drugName = d.drug.molecule_name;
-      const drugType = d.drug.molecule_type;
-      const phase = d.evidence.drug2clinic.max_phase_for_disease.numeric_index;
-      const status = d.evidence.drug2clinic.status;
-      const activity = MAP_ACTIVITY[d.target.activity];
-
-      return {
-        targetId,
-        targetSymbol,
-        targetClass,
-        efoId,
-        efoLabel,
-        drugId,
-        drugName,
-        drugType,
-        phase,
-        status,
-        activity,
-      };
-    }),
+    rows: rowsFormatted,
   };
 };
 
