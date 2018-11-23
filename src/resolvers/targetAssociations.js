9a899668585ebb4b46e0262aceced19caf8fd4a1
peatroot <garethpeat@gmail.com>
2018-11-23 09:44:09 +0000

    Use an enum for data types

--- src/resolvers/targetAssociations.js
+++ src/resolvers/targetAssociations.js
@@ -1,5 +1,15 @@
 import { targetAssociations } from "../apis/openTargets";
 
+const DATA_TYPE_MAP = {
+  genetic_association: "GENETIC",
+  somatic_mutation: "SOMATIC",
+  known_drug: "DRUG",
+  affected_pathway: "PATHWAY",
+  rna_expression: "EXPRESSION",
+  literature: "TEXT_MINING",
+  animal_model: "ANIMAL_MODEL",
+};
+
 const resolver = (obj, { ensgId }) => {
   return targetAssociations(ensgId).then(response => {
     const associations = response.data.data.map(d => {
@@ -8,11 +18,13 @@ const resolver = (obj, { ensgId }) => {
         name: d.disease.efo_info.label,
       };
       const score = 1.0 * d.association_score.overall;
-      const dataTypes = Object.keys(d.association_score.datatypes).map(dt => ({
-        id: dt,
-        name: dt.replace(/_/g, " "),
-        score: 1.0 * d.association_score.datatypes[dt],
-      }));
+      const dataTypes = Object.keys(d.association_score.datatypes).map(dt => {
+        const dataType = DATA_TYPE_MAP[dt];
+        return {
+          dataType,
+          score: 1.0 * d.association_score.datatypes[dt],
+        };
+      });
       return {
         disease,
         score,
