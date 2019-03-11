364d30cec1b431adf7d4cf344ba781da9f84ad28
Alfredo Miranda <alfredo@miranda.io>
2019-03-11 11:30:07 +0000

    Rename label to name

--- src/utils/getMultiplePublicationsSource.js
+++ src/utils/getMultiplePublicationsSource.js
@@ -2,11 +2,11 @@ const getMultiplePublicationsSource = pmIds => {
   if (pmIds.length === 0) {
     return null;
   }
-  const label = `${pmIds.length} publication${pmIds.length > 1 ? "s" : ""}`;
+  const name = `${pmIds.length} publication${pmIds.length > 1 ? "s" : ""}`;
   const url = `https://europepmc.org/search?query=${pmIds
     .map(pmId => (pmId.substring(0, 3) === "PMC" ? pmId : `EXT_ID:${pmId}`))
     .join(" OR ")}`;
-  return { label, url };
+  return { name, url };
 };
 
 export default getMultiplePublicationsSource;
