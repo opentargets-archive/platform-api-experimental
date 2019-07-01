fce6cf4d9df6e7434b8354c86b0a4701c6a0a242
peatroot <garethpeat@gmail.com>
2019-07-01 17:15:05 +0100

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -4,40 +4,6 @@ import _ from 'lodash';
 
 import apolloServerConfig from './public/apolloServerConfig';
 
-// import { typeDefs as Query, resolvers as resolversQuery } from './public/schema/Query';
-
-// import {
-//   typeDefs as Target,
-//   resolvers as resolversTarget,
-// } from './schema/Target';
-
-// import {
-//   typeDefs as Disease,
-//   resolvers as resolversDisease,
-// } from './public/schema/disease/Disease';
-
-// import {
-//   createTargetLoader,
-//   createExpressionLoader,
-//   createAtlasLoader,
-//   createGtexLoader,
-//   createTargetDrugsLoader,
-//   createDiseaseLoader,
-// } from './apis/dataloaders';
-
-// const server = new ApolloServer({
-//   typeDefs: [...Query, ...Target, ...Disease],
-//   resolvers: _.merge(resolversQuery, resolversTarget, resolversDisease),
-//   context: ({ req }) => ({
-//     targetLoader: createTargetLoader(),
-//     expressionLoader: createExpressionLoader(),
-//     atlasLoader: createAtlasLoader(),
-//     gtexLoader: createGtexLoader(),
-//     diseaseLoader: createDiseaseLoader(),
-//     targetDrugsLoader: createTargetDrugsLoader(),
-//   }),
-// });
-
 const server = new ApolloServer(apolloServerConfig);
 
 const app = express();
