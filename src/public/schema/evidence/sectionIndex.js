c3618b999423e0c4fbb2773f7179a37564cffa95
peatroot <garethpeat@gmail.com>
2019-07-15 11:46:16 +0100

    Add animal models evidence to api

--- src/public/schema/evidence/sectionIndex.js
+++ src/public/schema/evidence/sectionIndex.js
@@ -1,10 +1,12 @@
 // TODO: see if this can be done dynamically
 //       or try @babel/plugin-proposal-export-namespace-from (may need eject)
 
+import * as animalModelsRaw from './sections/AnimalModels';
 import * as differentialExpressionRaw from './sections/DifferentialExpression';
 import * as drugsRaw from './sections/Drugs';
 import * as pathwaysRaw from './sections/Pathways';
 
+export const animalModels = animalModelsRaw;
 export const differentialExpression = differentialExpressionRaw;
 export const drugs = drugsRaw;
 export const pathways = pathwaysRaw;
