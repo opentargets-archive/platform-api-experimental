24561a96eba504c05356f471b6ce1b8ac0384572
Luca Fumis <lucaf@ebi.ac.uk>
2019-12-13 12:59:11 +0000

    Add indications definition and resolvers; issue opentargets/platform#773

--- src/public/schema/drug/sections/LinkedDiseases.js
+++ src/public/schema/drug/sections/LinkedDiseases.js
@@ -1,15 +1,6 @@
 import { gql } from 'apollo-server-express';
 
-import { drugDiseases } from '../../../apis/openTargets';
-
-import therapeuticAreasPerDisease from '../../disease/therapeuticAreasPerDisease';
-
 export const id = 'linkedDiseases';
-
-// TODO: We should NOT use `drugDiseases`, which essentially looks for
-//       evidence containing the drug name. There will be a new field
-//       `indications` in the 19.09 drug index, which we should switch
-//       to when available.
 
 export const summaryTypeDefs = gql`
   type DrugSummaryLinkedDiseases {
@@ -21,7 +12,9 @@ export const summaryTypeDefs = gql`
 export const summaryResolvers = {
   DrugSummaryLinkedDiseases: {
     linkedDiseaseCount: ({ _chemblId }, args, { drugLoader }) =>
-      drugLoader.load(_chemblId).then(({ indications }) => indications.length),
+      drugLoader
+        .load(_chemblId)
+        .then(({ indicationsCount }) => indicationsCount),
     sources: () => [
       {
         name: 'ChEMBL',
@@ -33,22 +26,24 @@ export const summaryResolvers = {
 
 export const sectionTypeDefs = gql`
   type DrugDetailLinkedDiseases {
-    rows: [Disease!]!
+    rows: [Indication!]!
   }
 `;
 
 export const sectionResolvers = {
   DrugDetailLinkedDiseases: {
     rows: ({ _chemblId }, args, { drugLoader }) =>
-      drugLoader
-        .load(_chemblId)
-        .then(({ name }) => drugDiseases(name))
-        .then(rows =>
-          rows.map(({ id, name }) => ({
-            _efoId: id, // needed for other disease resolvers
-            id,
-            name,
-          }))
-        ),
+      drugLoader.load(_chemblId).then(({ indications }) => {
+        return indications.map(
+          ({ efo_id, efo_label, max_phase_for_indication }) => {
+            return {
+              _efoId: efo_id, // needed for other resolvers
+              id: efo_id,
+              name: efo_label,
+              maxPhase: max_phase_for_indication,
+            };
+          }
+        );
+      }),
   },
 };
